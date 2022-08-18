ee93d4a1be48b661f57562f9c37d353fffc7f3fe
ismixan <ismixan1996@mail.ru>
2022-08-18 20:18:16 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+
+// let x = window.prompt('Eded daxil edin')
+
+//  let sum = 0;
+//  while(count < x) {
+//     sum = sum + count; 
+//      count++; 
+//  }
+//  console.log('sum', sum)
+
+
+//let sum = 0:
+//for(let count =0 ; count<Element; count++) {
+   // sum += count;
+//console.log(count +2)
+   //if(count %2 === 0){
+//}
+let value=window.prompt('eded daxil Element')
+
+value=parseFloat(value)
+
+
+
+if(value===1) {
+    console.log('yanvar')}
+
+
